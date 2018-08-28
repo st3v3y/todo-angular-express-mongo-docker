44c87d76c571246abdf17b2692de731296b20c17
Stefan Lessle <stefan.lessle@web.de>
2018-08-28 19:43:39 +0700

    installed angular material and put basic content

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule,
+        MatCheckboxModule, MatListModule, MatCardModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [BrowserAnimationsModule],
+    [MatCardModule, MatListModule, MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule, MatCheckboxModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
